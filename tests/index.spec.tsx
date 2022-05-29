1c0e619475909e9ce03836cff886dfec6ce11746
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-29 13:51:28 +0900

    テストを移動した
    pagesの下にあるとbuildのときにエラーになるため

--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
-import Home from "../index";
+import Home from "../pages/index";
 
 describe("トップページ", () => {
   beforeEach(() => {
