000fae864b2b9b168a745fde62d33d2ca21df032
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-29 15:35:36 +0900

    登録ブロックのテストを並べた

---
+++ tests/registerHook.spec.tsx
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("登録ブロックのアクション", () => {
+  it.todo("初期状態では登録するテキストの入力領域は空である。");
+  it.todo("登録するテキストが空でない場合、登録ボタンが有効である。");
+  it.todo(
+    "登録ボタンを押すと、登録するテキストに入力されたテキストが登録されたテキストに表示される。"
+  );
+});
