8330970ccbe610983f5616eb85ee926f4d29039c
kubotama <16697573+kubotama@users.noreply.github.com>
2022-05-31 23:00:21 +0900

    変換ブロックのテストを並べた

---
+++ tests/ttsHook.spec.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom";
+import { renderHook, act } from "@testing-library/react-hooks";
+
+describe("変換ブロックのカスタムフックのテスト", () => {
+  it.todo("初期状態では登録されたテキストの表示領域が空である。");
+  it.todo("初期状態では変換ボタンが無効である。");
+  it.todo("登録されたテキストが空でない場合、変換ボタンが有効である。");
+  it.todo(
+    "変換ボタンを押すと、変換されたテキストが変換されたテキストに表示される。"
+  );
+});
