c4ada967d3421250144bacc718cc2609b0b280de
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-03 13:34:32 +0900

    テストの説明を修正した

--- tests/tts.spec.tsx
+++ tests/tts.spec.tsx
@@ -3,10 +3,8 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe("変換ブロックのカスタムフックのテスト", () => {
-  it.todo("初期状態では登録されたテキストの表示領域が空である。");
+  it.todo("初期状態では変換されたテキストの表示領域が空である。");
   it.todo("初期状態では変換ボタンが無効である。");
   it.todo("登録されたテキストが空でない場合、変換ボタンが有効である。");
-  it.todo(
-    "変換ボタンを押すと、変換されたテキストが変換されたテキストに表示される。"
-  );
+  it.todo("変換ボタンを押すと変換されたテキストに表示される。");
 });
