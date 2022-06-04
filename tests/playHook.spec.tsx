aaec4a07dbf0f0a0a9ed7ae11b5e1c75fbea521e
kubotama <16697573+kubotama@users.noreply.github.com>
2022-06-04 22:57:27 +0900

    refact: 冗長な変数を削除した

--- tests/playHook.spec.tsx
+++ tests/playHook.spec.tsx
@@ -7,8 +7,7 @@ import { useTts } from "../hooks/ttsHook";
 describe("再生ブロックのカスタムフックのテスト", () => {
   it("初期状態では再生ボタンは無効である", () => {
     const { result } = renderHook(() => useTts("テキスト"));
-    const playButtonDisabled = result.current.playButtonDisabled;
-    expect(playButtonDisabled).toEqual(true);
+    expect(result.current.playButtonDisabled).toEqual(true);
   });
 
   it("変換されたテキストが設定されると再生ボタンが有効になる", () => {
@@ -16,8 +15,7 @@ describe("再生ブロックのカスタムフックのテスト", () => {
     act(() => {
       result.current.onClickTts();
     });
-    const playButtonDisabled = result.current.playButtonDisabled;
-    expect(playButtonDisabled).toEqual(false);
+    expect(result.current.playButtonDisabled).toEqual(false);
   });
 
   it.todo("再生ボタンを押すと割り当てられた関数が呼び出される");
